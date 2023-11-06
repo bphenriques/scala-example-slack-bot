fe550eaccdc36e06ed2e3209e7724ca8dd0c1aad
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2023-11-06 16:00:28 +0000

    Simplify

--- modules/app/src/main/scala/com/bphenriques/example/slack/Main.scala
+++ modules/app/src/main/scala/com/bphenriques/example/slack/Main.scala
@@ -6,13 +6,13 @@ import cats.effect.{IO, IOApp, Resource}
 import com.bphenriques.example.slack.http.HttpServer
 import com.bphenriques.example.slack.services.{SantaClausService, SlackBot}
 import com.bphenriques.example.slack.slack.{Http4sSlackProxy, Slack}
-import com.slack.api.bolt.{App, AppConfig}
+import com.slack.api.bolt.AppConfig
+import org.typelevel.log4cats.StructuredLogger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
-import org.typelevel.log4cats.{Logger, StructuredLogger}
 
 object Main extends IOApp.Simple {
 
-  override def run: IO[Unit] = resources.use { case (httpServer, _) => httpServer.run }
+  override def run: IO[Unit] = resources.use(httpServer => httpServer.run)
 
   private def loggerR: Resource[IO, StructuredLogger[IO]] =
     Resource
@@ -22,21 +22,18 @@ object Main extends IOApp.Simple {
       }
       .evalTap(logger => logger.info(s"Slack Bot server started"))
 
-  private def resources: Resource[IO, (HttpServer, Logger[IO])] = for {
+  private def resources: Resource[IO, HttpServer] = for {
     config <- Resource.eval(Config.value.load[IO])
     logger <- loggerR
-    service         = SantaClausService(UUIDGen[IO].randomUUID.map(_.toString), IO.realTimeInstant)(logger)
+    service = SantaClausService(UUIDGen[IO].randomUUID.map(_.toString), IO.realTimeInstant)(logger)
     slackBot <- Slack.make(config.slack.slackBotToken).map(SlackBot.make(_, service)(logger))
-    slackServerApp <- Resource.eval(
-      IO(
-        AppConfig
-          .builder()
-          .singleTeamBotToken(config.slack.slackBotToken)
-          .signingSecret(config.slack.signingKey)
-          .build(),
-      ).map(slackConfig => new App(slackConfig)),
+    http4sSlackProxy = Http4sSlackProxy.make[IO](
+      AppConfig
+        .builder()
+        .singleTeamBotToken(config.slack.slackBotToken)
+        .signingSecret(config.slack.signingKey)
+        .build(),
     )
-    http4sSlackProxy = Http4sSlackProxy.make[IO](slackServerApp.config())
-    server           = new HttpServer(slackBot, http4sSlackProxy)(logger)
-  } yield (server, logger)
+    server = new HttpServer(slackBot, http4sSlackProxy)(logger)
+  } yield server
 }
